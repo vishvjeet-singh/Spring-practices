228e7765762fd32099fe765222716ff9117c7a98
vishvjeet <vishvjeetsinghrathore751@gmail.com>
2023-10-31 14:09:05 +0530

    spring life initial

---
+++ spring-life/src/main/java/com/springs/spring_life/App.java
@@ -0,0 +1,13 @@
+package com.springs.spring_life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
