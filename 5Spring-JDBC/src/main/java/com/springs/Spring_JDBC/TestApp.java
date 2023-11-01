2725cc5e0ed31d355266d35b1764da2d03c90b90
vishvjeet <vishvjeetsinghrathore751@gmail.com>
2023-11-01 15:17:57 +0530

    CRUD operations

--- 5Spring-JDBC/src/main/java/com/springs/Spring_JDBC/TestApp.java
+++ 5Spring-JDBC/src/main/java/com/springs/Spring_JDBC/TestApp.java
@@ -8,11 +8,30 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class TestApp {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springs/Spring_JDBC/config.xml");
 
 		JdbcTemplate jd =  context.getBean("jdbcTemplate",JdbcTemplate.class);
-		System.out.println(jd);
+		System.out.println(jd.getDataSource().getConnection());
+		
+		//insert
+		/*
+		 * String sql = "insert into student(id, Name, address) values(?,?,?)"; int i=
+		 * jd.update(sql,2,"vishvjeet","laduna");
+		 * System.out.println("successfully inserted :"+i);
+		 */
+		
+		//update
+		/*
+		 * String sql ="update student set Name=?, address=? where id=?"; int i=
+		 * jd.update(sql, "kuku","noida",2); System.out.println("updated"+i);
+		 */
+		
+		//delete
+		String sql ="delete from student where id=?";
+		int i=jd.update(sql,2);
+		System.out.println("deleted : " +i);
+		
 	}
 
 }
